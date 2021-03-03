cfc069738229d9a96f29b76d44127eeb0f0594cf
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 11:49:04 +0000

    two variables asking for string input. Conditional print command.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    first_string = str(input("Enter the first string:"))
+    second_string = str(input("Enter the second string:"))
+
+    if(first_string==second_string):
+      print("Same")
+    else:
+      print("Different")
 
 if __name__ == '__main__':
     main()
